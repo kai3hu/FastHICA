34401290f8327a79661c124ed0cffdcd44df8560
shu kai <shukai@shus-MacBook-Air.local>
2022-11-03 18:16:06 +0900

    CoverByLineIndex

--- fastCA/minisat/solver/Solver.d
+++
@@ -1,4 +0,0 @@
-minisat/solver/Solver.o minisat/solver/Solver.d:	 minisat/solver/Solver.C minisat/solver/Solver.H \
- minisat/include/Vec.h minisat/include/Heap.h minisat/include/Vec.h \
- minisat/include/Alg.h minisat/solver/SolverTypes.H \
- minisat/include/Sort.h
